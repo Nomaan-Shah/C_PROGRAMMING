a7241095a64784ccceda41be2022c374985d1d68
Nomaan-Shah <165920780+Nomaan-Shah@users.noreply.github.com>
2024-05-15 19:59:49 +0530

    Add files via upload

---
+++ 32/size of.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	printf("\nSize of int: %lu bytes\n",sizeof(int));
+	printf("\nSize of float: %lu bytes\n",sizeof(float));
+	printf("\nSize of char: %lu bytes\n",sizeof(char));
+	return 0;
+}
