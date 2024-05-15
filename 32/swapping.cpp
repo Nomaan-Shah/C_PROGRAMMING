0485872430be4ab4b3700288b1702ae53c74a327
Nomaan-Shah <165920780+Nomaan-Shah@users.noreply.github.com>
2024-05-15 20:31:26 +0530

    Add files via upload

---
+++ 32/swapping.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	a=10,b=20;
+	printf("\n before swapping: a=%d b=%d",a,b);
+	c=a;
+	a=b;
+	b=c;
+	printf("\n after swapping: a=%d b=%d",a,b);
+	return 0;
+}
